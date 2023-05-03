7e02c2824b6c64f397a29f9171c173174bc5e45c
shukumar-542 <shukumar542@gmail.com>
2023-05-03 13:29:48 +0600

    update process env port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 
 const cors = require('cors')
 app.use(cors())
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
